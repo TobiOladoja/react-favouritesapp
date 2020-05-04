c1854c6f154aac486f9cc9f8e80dd33992f2159b
TobiOladoja <tbfullstack@gmail.com>
2020-05-04 08:41:55 +0100

    Components folder grouped

---
+++ src/components/AddFav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AddFav() {
+  return (
+    <main>
+      <div className='leftContent'>
+        <img src='https://miro.medium.com/max/863/1*BFV8Gwt5BILa-xv04IK2ng.png' />
+        <form>
+          <h2 className='formTitle'>Add a Favourite</h2>
+          div
+        </form>
+      </div>
+      <div className='rightContent'></div>
+    </main>
+  );
+}
+
+export default AddFav;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav classsName='navigation'>
+      <a>
+        <img src='https://miro.medium.com/max/863/1*BFV8Gwt5BILa-xv04IK2ng.png' />
+      </a>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
